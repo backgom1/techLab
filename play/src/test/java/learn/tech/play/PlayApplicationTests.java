098089f126872310c3aaad7d6160d9626f4229ab
eunseong <dmstjd0214@naver.com>
2025-05-11 17:10:15 +0900

    프로젝트 생성

---
+++ play/src/test/java/learn/tech/play/PlayApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.tech.play;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
